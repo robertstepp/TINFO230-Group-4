c810fcdd7dbd7858c9a8e8b54333dd6018b3037d
Robert Stepp <robert@vyperinc.ninja>
2020-02-20 09:22:01 -0800

    Working on navigation and made base home file

--- js/common.js
+++ js/common.js
@@ -1,1 +1,10 @@
 // TODO: Navigation bar with highlighting
+window.onload = function navigation() {
+    this.document.getElementById("navigation").innerHTML = 
+        '<li class="home"><a href="home.html" class="home">Home</a></li>' + 
+        '<li class="products"><a href="products.html" class="products">Products</a></li>' + 
+        '<li class="location"><a href="location.html" class="location">Location</a></li>' + 
+        '<li class="mission"><a href="mission.html" class="mission">Mission</a></li>' + 
+        '<li class="contactUs"><a href="contactus.html" class="contactUs">Contact Us</a></li>' + 
+        '<li class="aboutUs"><a href="aboutus.html" class="aboutUs">About Us</a></li>';
+}
